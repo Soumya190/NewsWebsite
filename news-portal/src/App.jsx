a43eb00fb0e82d967a59202628645f0535215d20
soumya tiwari <soumyatiwari7866@gmail.com>
2025-04-26 05:21:56 +0530

    Fix build issues

--- news-portal/src/App.jsx
+++ news-portal/src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage.jsx";
-// import ArticlePage from "./pages/ArticlePage.jsx";
-// import  Pagination from "./pages/Pagination.jsx";
 
 function App() {
   return (
